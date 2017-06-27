1f58f295be2ed565806aeccfa6b6f43be0f04c5c
nielongguang <627460326@qq.com>
2017-06-27 09:00:03 +0800

    上传部分完成

--- src/main/java/com/cn/study/basicSpring/config/UploadController.java
+++ src/main/java/com/cn/study/basicSpring/config/UploadController.java
@@ -1,9 +1,15 @@
 package com.cn.study.basicSpring.config;
 
+import ch.qos.logback.core.util.FileUtil;
+import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by Administrator on 2017/6/26.
@@ -12,7 +18,19 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller
 public class UploadController {
 
-    public String upload(MultipartFile file) {
-        return "worong";
+    @RequestMapping(value = "/upload",method = RequestMethod.POST)
+    public @ResponseBody String upload(MultipartFile file) {
+        try {
+            FileUtils.writeByteArrayToFile(
+                    new File("E:\\upload\\" + file.getOriginalFilename()),
+                    file.getBytes());
+
+            return "ok";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "worong";
+        }
+
+
     }
 }
